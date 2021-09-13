23b289bed2330da829f514a4c8120a6eb0d39ffa
hchen657 <hchen657@gatech.edu>
2021-09-13 11:04:11 -0400

    code and comment

---
+++ env_params.py
@@ -0,0 +1,44 @@
+params = {
+  "in_bounds": {
+    "x_bounds": [
+      -1.0, 
+      1.0
+    ], 
+    "y_bounds": [
+      -1.2, 
+      1.2
+    ]
+  }, 
+  "noise_sigma": 0.03, 
+  "min_dist": 0.03, 
+  "_args": {
+    "noise_sigma": 0.03, 
+    "min_dist": 0.03, 
+    "asteroid_b_max": 0.01, 
+    "craft_angle_increment": 0.39269908169872414, 
+    "craft_max_speed": 0.03, 
+    "t_step": 2, 
+    "asteroid_a_max": 0.0001, 
+    "t_future": 1000, 
+    "t_past": -100
+  }, 
+  "initial_craft_state": {
+    "x": 0.0, 
+    "max_speed": 0.02, 
+    "speed_increment": 0.01, 
+    "v": 0.01, 
+    "y": -1.0, 
+    "h": 1.5707963267948966, 
+    "angle_increment": 0.39269908169872414
+  }, 
+  "goal_bounds": {
+    "x_bounds": [
+      -1.0, 
+      1.0
+    ], 
+    "y_bounds": [
+      1.0, 
+      1.2
+    ]
+  }
+}
--- test_one.py
+++ test_one.py
@@ -26,9 +26,9 @@ import collections
 import asteroid
 import bounds
 import craft
-import cases
 import simu_env
 import runner
+import param
 import tensorflow as tf
 from tensorflow import keras
 from turtle_display import TurtleRunnerDisplay
@@ -37,7 +37,6 @@ from td3 import TD3
 from pe_model import PE
 from fake_env import FakeEnv
 from ssa import SafeSetAlgorithm
-from cbf import ControlBarrierFunction
 '''
 random.seed(1)
 np.random.seed(1)
@@ -90,18 +89,12 @@ def run_kwargs( params ):
 
 def parser():
     prsr = argparse.ArgumentParser()
-    prsr.add_argument( 'method',
-                       help="Which method to test",
-                       type=str,
-                       choices=('estimate', 'navigate'),
-                       default='estimate')
-    prsr.add_argument( '--case',
-                       help="test case number (one of %s) or test case file" % list(cases.index.keys()),
-                       type=str,
-                       default=1)
     prsr.add_argument( '--display',
                        choices=('turtle','text','none'),
                        default='none' )
+    prsr.add_argument( '--explore',
+                   choices=('psn','rnd','none'),
+                   default='none' )
     prsr.add_argument( '--qp',dest='is_qp', action='store_true')
     prsr.add_argument( '--no-qp',dest='is_qp', action='store_false')
     prsr.add_argument( '--ssa-buffer',dest='enable_ssa_buffer', action='store_true')
@@ -109,10 +102,10 @@ def parser():
     #parser.set_defaults(is_qp=True)
     return prsr
 
-def main(method_name, case_id, display_name, qp, enable_ssa_buffer):
+def main(display_name, exploration, qp, enable_ssa_buffer):
     # testing env
     try:
-        params = cases.index[ int(case_id) ]
+        params = param.params
     except Exception as e:
         print(e)
         return
@@ -126,18 +119,16 @@ def main(method_name, case_id, display_name, qp, enable_ssa_buffer):
     state_dim = craft_state_size + nearest_obstacle_state_size
 
     model_update_freq = 1000
-    env_model = PE(state_dim = craft_state_size, action_dim = craft_action_size) #Dynamics model
+    env_model = PE(state_dim = craft_state_size, action_dim = craft_action_size) 
     fake_env = FakeEnv(env_model) # FakeEnv to help model unrolling
     env = simu_env.Env(display, fake_env, **(env_params))
 
     policy_replay_buffer = ReplayBuffer(state_dim = state_dim, action_dim = craft_action_size, max_size=int(1e6))
     policy = TD3(state_dim, craft_action_size, env.max_acc, env.max_acc)
-    #policy.load("rl_model/rl")
     ssa_replay_buffer = ReplayBuffer(state_dim = state_dim, action_dim = craft_action_size, max_size=int(1e6))
     # ssa
     safe_controller = SafeSetAlgorithm(max_speed = env.craft_state.max_speed, fake_env = fake_env, is_qp = qp)
-    # cbf
-    # safe_controller = ControlBarrierFunction()
+
     # parameters
     max_steps = int(1e6)
     start_timesteps = 2e3
@@ -164,18 +155,14 @@ def main(method_name, case_id, display_name, qp, enable_ssa_buffer):
     is_meet_requirement = False
     reward_records = []
 
-    robot_xs = []
-    robot_ys = []
-    obs_xs = []
-    obs_ys = []
-    safe_obs_xs = []
-    safe_obs_ys = []
     for t in range(max_steps):
-      #if (env.cur_step == 0):
-      #  policy.parameter_explore()
-      #  print(f"parameter_explore in {t}")
-      '''
-      if (t > 1024):
+      # disturb the policy parameters at beginning of each episodes when using PSN
+      if (exploration == 'psn' and env.cur_step == 0):
+        policy.parameter_explore()
+        print(f"parameter_explore in {t}")
+      
+      # train the random network prediction when using rnd
+      if (exploration == 'rnd' and t > 1024):
         with tf.GradientTape() as tape:
           state_batch, action_batch, next_state_batch, reward_batch, not_done_batch =  policy_replay_buffer.sample(256)
           q_fixed = rnd_fixed.call(state_batch)
@@ -183,62 +170,47 @@ def main(method_name, case_id, display_name, qp, enable_ssa_buffer):
           loss = rnd_loss(q_fixed, q_train)
           gradients = tape.gradient(loss, rnd_train.trainable_weights)
           rnd_optimizer.apply_gradients(zip(gradients, rnd_train.trainable_weights))
-      '''
+      
       action = policy.select_action(state)
-      #if (t % 50 == 0):
-      #  print(action)
       env.display_start()
       # ssa parameters
       unsafe_obstacle_ids, unsafe_obstacles = env.find_unsafe_obstacles(env.min_dist * 6)
       original_action = action
       action, is_safe, is_unavoidable, danger_obs = safe_controller.get_safe_control(state[:4], unsafe_obstacles, fx, gx, action)
-      '''
-      if (len(danger_obs) > 0):
-        for obs in danger_obs:
-          obs_xs.append(obs[0])
-          obs_ys.append(obs[1])
-
-      for obs in env.field.asteroids:
-        safe_obs_xs.append(obs.c_x)
-        safe_obs_ys.append(obs.c_y)
-      '''
       #is_safe = False
       # take safe action
-      #robot_xs.append(state[0])
-      #robot_ys.append(state[1])
       s_new, reward, done, info = env.step(action, is_safe, unsafe_obstacle_ids) 
       original_reward = reward
-      # RND
-      '''
-      rnd_state = tf.convert_to_tensor(state.reshape(1, -1))
-      q_fixed = rnd_fixed.call(rnd_state)
-      q_train = rnd_train.call(rnd_state)                    
-      loss = np.sum(np.square(q_fixed - q_train))      
-      reward += loss
-      '''
       episode_reward += original_reward
+      # add the novelty to reward when using rnd
+      if (exploration == 'rnd'):
+        rnd_state = tf.convert_to_tensor(state.reshape(1, -1))
+        q_fixed = rnd_fixed.call(rnd_state)
+        q_train = rnd_train.call(rnd_state)                    
+        loss = np.sum(np.square(q_fixed - q_train))      
+        reward += loss      
+      
       env.display_end()
       # Store data in replay buffer
-      # Every SSA + RL should use safe action to train RL policy
       if (enable_ssa_buffer):
         if (is_safe):
           ssa_replay_buffer.add(state, action, s_new, reward, done)          
         else:
           policy_replay_buffer.add(state, action, s_new, reward, done)
       else:
-        #print("here")
-        policy_replay_buffer.add(state, original_action, s_new, reward, done) # 直接把safe u拿来学习可以么
+        policy_replay_buffer.add(state, original_action, s_new, reward, done)
       state = s_new
+      
+      # train policy
       if (policy_replay_buffer.size > 1024):
         state_batch, action_batch, next_state_batch, reward_batch, not_done_batch =  [np.array(x) for x in policy_replay_buffer.sample(256)]
         if enable_ssa_buffer and ssa_replay_buffer.size > 128:
-            model_batch_size = int(0.4*256)
+            model_batch_size = int(0.4*256) # batch size is 256, ratio is 0.4
             idx = np.random.choice(256, model_batch_size, replace=False)
             state_batch[idx], action_batch[idx], next_state_batch[idx], reward_batch[idx], not_done_batch[idx] =  ssa_replay_buffer.sample(model_batch_size)
         policy.train_on_batch(state_batch, action_batch, next_state_batch, reward_batch, not_done_batch)
 
-      if (done and original_reward == -500):   
-        #print("collision")         
+      if (done and original_reward == -500):          
         #print(safe_controller.records) 
         collision_num += 1        
       elif (done and original_reward == 2000):
@@ -248,51 +220,32 @@ def main(method_name, case_id, display_name, qp, enable_ssa_buffer):
       
       if (done):      
         total_steps += env.cur_step
-        print(f"Train: episode_num {episode_num}, total_steps {total_steps}, reward {episode_reward}, is_qp {qp}, last state {state[:4]}")
+        print(f"Train: episode_num {episode_num}, total_steps {total_steps}, reward {episode_reward}, is_qp {qp}, exploration {exploration}, last state {state[:4]}")
         total_rewards.append(episode_reward)
         episode_reward = 0
         episode_num += 1
         state, done = env.reset(), False
-        '''
-        if (qp):
-          np.save('trajectory_result/xs_qp.npy', np.array(robot_xs))
-          np.save('trajectory_result/ys_qp.npy', np.array(robot_ys))
-          np.save('trajectory_result/obs_xs_qp.npy', np.array(obs_xs))
-          np.save('trajectory_result/obs_ys_qp.npy', np.array(obs_ys))
-        else:
-          np.save('trajectory_result/xs.npy', np.array(robot_xs))
-          np.save('trajectory_result/ys.npy', np.array(robot_ys))
-          np.save('trajectory_result/obs_xs.npy', np.array(obs_xs))
-          np.save('trajectory_result/obs_ys.npy', np.array(obs_ys))
-          np.save('trajectory_result/safe_obs_xs.npy', np.array(safe_obs_xs))
-          np.save('trajectory_result/safe_obs_ys.npy', np.array(safe_obs_ys))
 
-        robot_xs = []
-        robot_ys = []
-        obs_xs = []
-        obs_ys = []
-        safe_obs_xs = []
-        safe_obs_ys = []
-        '''
-      #if (len(total_rewards) >= 20 and np.mean(total_rewards[-20:]) >= 1900 and not is_meet_requirement):
-      #  print(f"\n\n\nWe meet the reward threshold episode_num {episode_num}, total_steps {total_steps}\n\n\n")
-      #  is_meet_requirement = True
-      #  break
-      #if (episode_num >= 50):
-      #  print(f"success_num {success_num}, failure_num {failure_num}, collision_num {collision_num}, avg_reward {np.mean(total_rewards)}")
-      #  break
-      
+      # check reward threshold
+      '''
+      if (len(total_rewards) >= 20 and np.mean(total_rewards[-20:]) >= 1900 and not is_meet_requirement):
+        print(f"\n\n\nWe meet the reward threshold episode_num {episode_num}, total_steps {total_steps}\n\n\n")
+        is_meet_requirement = True
+        break
+      '''
+
+      # evalution part at every 1000 steps
+      '''
       if (t % 1000 == 0):
-        #print(f"avg reward {np.mean(total_rewards)}, success {success_num}, failure {failure_num}, collision {collision_num}")
         env.save_env()
         eval_reward = eval(policy, env, safe_controller, fx, gx)
         print(f"t {t}, eval_reward {eval_reward}")
         reward_records.append(eval_reward)
         env.read_env()
-        #print(reward_records)
         if (len(reward_records) == 100):
           break
-      
+      '''
+
     return reward_records
 
 def eval(policy, env, safe_controller, fx, gx):
@@ -318,9 +271,8 @@ if __name__ == '__main__':
     for i in range(100):
       all_reward_records.append([])
     for i in range(10):
-      reward_records = main( method_name  = args.method,
-          case_id      = args.case,
-          display_name = args.display,
+      reward_records = main(display_name = args.display, 
+          exploration = args.explore,
           qp = args.is_qp,
           enable_ssa_buffer = args.enable_ssa_buffer)
       for j, n in enumerate(reward_records):
