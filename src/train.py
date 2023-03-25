2ac9b91ec455805987f1435ecf02937bced36542
Hongyi Chen <59629242+hychen-naza@users.noreply.github.com>
2023-03-24 23:15:23 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,17 +1,6 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
-######################################################################
-# This file copyright the Georgia Institute of Technology
-#
-# Permission is given to students to use or modify this file (only)
-# to work on their assignments.
-#
-# You may NOT publish this file or make it available to others not in
-# the course.
-#
-######################################################################
-
 # python modules
 import argparse
 import importlib
